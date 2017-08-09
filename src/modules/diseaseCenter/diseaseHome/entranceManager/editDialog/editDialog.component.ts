78e3f51df792a182c1f38cdfe080c09df50704a0
wangzhixiang <wangzhixiang@jk.pingan-inc.cn>
2017-08-09 10:28:59 +0800

    入口编辑模块增加init的钩子

--- src/modules/diseaseCenter/diseaseHome/entranceManager/editDialog/editDialog.component.ts
+++ src/modules/diseaseCenter/diseaseHome/entranceManager/editDialog/editDialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core'
+import { Component, Inject, OnInit } from '@angular/core'
 import { MdDialogRef, MD_DIALOG_DATA } from '@angular/material'
 import { FormGroup, FormBuilder, Validators } from '@angular/forms'
 import { IDiseaseCenter } from 'root/src/models'
@@ -9,15 +9,16 @@ import { EntranceEditService } from 'root/src/services'
   templateUrl: './editDialog.component.html',
   styleUrls: ['./editDialog.component.css']
 })
-export class EntranceEdit{
+export class EntranceEdit implements OnInit{
   entranceEditForm: FormGroup
   constructor(
     @Inject(MD_DIALOG_DATA) public formData: IDiseaseCenter,
     private fb: FormBuilder,
     private entranceService: EntranceEditService,
     public dialogRef: MdDialogRef<EntranceEdit>
-  ){
-    console.log(entranceService.getEntranceEditData(), this.formData)
+  ){}
+
+  ngOnInit(): void {
     const { sortFactor, name, linkUrl, icon, poster, channels } = this.formData
     this.entranceEditForm = this.fb.group({
       sortFactor: [sortFactor, Validators.required],
